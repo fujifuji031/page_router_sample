49170be5d0b8f671ed6b4033d0c7c35777ac99bf
fujifuji031 <106134395+fujifuji031@users.noreply.github.com>
2022-08-22 17:09:52 +0900

    Initial commit

---
+++ src/router/Router.jsx
@@ -0,0 +1,56 @@
+import { Switch, Route } from "react-router-dom";
+import { Home } from "../Home";
+import { page1Routes } from "./Page1Routers";
+import { page2Routes } from "./Page2Routers";
+
+export const Router = () => {
+  return (
+    <Switch>
+      {/**exactをつけないと部分一致になってHomeしか対象にならない */}
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route
+        path="/page1"
+        render={({ match: { url } }) => (
+          <Switch>
+            {page1Routes.map((route) => (
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                {route.children}
+              </Route>
+            ))}
+            {/* <Route exact path={url}>
+              <Page1 />
+            </Route>
+            <Route path={`${url}/detailA`}>
+              <Page1DetailA />
+            </Route>
+            <Route path={`${url}/detailB`}>
+              <Page1DetailB /> */}
+            {/* </Route> */}
+          </Switch>
+        )}
+      />
+      <Route
+        path="/page2"
+        render={({ match: { url } }) => (
+          <Switch>
+            {page2Routes.map((route) => (
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                {route.children}
+              </Route>
+            ))}
+          </Switch>
+        )}
+      />
+    </Switch>
+  );
+};
